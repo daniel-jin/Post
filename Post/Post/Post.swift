43e776c7045a174e7204a4b4041bbee626f59a17
daniel-jin <daniel.h.jin@gmail.com>
2018-01-12 01:39:22 -0600

    Implemented Post object model

---
+++ Post/Post/Post.swift
@@ -0,0 +1,26 @@
+//
+//  Post.swift
+//  Post
+//
+//  Created by Daniel Jin on 1/12/18.
+//  Copyright © 2018 Daniel Jin. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    init(username: String, text: String, timestamp: TimeInterval = Date().timeIntervalSince1970) {
+        
+        self.username = username
+        self.text = text
+        self.timestamp = timestamp
+        
+    }
+    
+    // MARK: - Properties
+    let username: String
+    let text: String
+    let timestamp: TimeInterval
+    
+}
