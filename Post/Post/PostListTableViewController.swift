b9e657c5f7bc7aea803bb239c81bc097798ddb42
daniel-jin <daniel.h.jin@gmail.com>
2018-01-13 17:55:05 -0600

    Table view setup

--- Post/Post/PostListTableViewController.swift
+++ Post/Post/PostListTableViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class PostListTableViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var postController = PostController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,24 +22,21 @@ class PostListTableViewController: UITableViewController {
     
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return postController.posts.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
-
+        let post = postController.posts[indexPath.row]
+        
+        cell.textLabel?.text = post.text
+        cell.detailTextLabel?.text = "\(indexPath.row) - \(post.username) - \(Date(timeIntervalSince1970: post.timestamp))"
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
